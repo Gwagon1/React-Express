aceb2350cfb28bf4d5854a71be3ec9f755cd838e
Gryphon <bekaardt21@gmail.com>
2024-11-06 15:48:19 +0200

    Updated start script. Updated githubRoutes.js

--- backend/routes/githubRoutes.js
+++ backend/routes/githubRoutes.js
@@ -5,6 +5,21 @@ const router = express.Router();
 // Routes to interact with GitHub API
 router.get('/:username', getUser);
 router.get('/:username/repos', getUserRepos);
+router.get('/:username/repos/:repo', async (req, res) => {
+  const { username, repo } = req.params;
+  const githubApiUrl = `https://api.github.com/repos/${username}/${repo}`;
+
+  try {
+    const response = await fetch(githubApiUrl);
+    if (!response.ok) {
+      return res.status(404).json({ message: 'Repository not found' });
+    }
+    const repoData = await response.json();
+    res.json(repoData);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching repo data', error });
+  }
+});
 router.get('/:username/repos/:repo/commits', getRepoCommits);
 
 module.exports = router;
--- backend/server.mjs
+++ backend/server.mjs
@@ -1,9 +1,9 @@
-// Import necessary modules
+// server.mjs
+
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import githubRoutes from './routes/githubRoutes.js';
-import fetch from 'node-fetch';
 import path from 'path';
 
 const app = express();
@@ -16,30 +16,13 @@ app.use(express.json());
 // Route for GitHub API interactions
 app.use('/api/users', githubRoutes);
 
-// API route for specific repo data
-app.get('/api/repos/:owner/:repo', async (req, res) => {
-  const { owner, repo } = req.params;
-  const githubApiUrl = `https://api.github.com/repos/${owner}/${repo}`;
-
-  try {
-    const response = await fetch(githubApiUrl);
-    if (!response.ok) {
-      return res.status(404).json({ message: 'Repository not found' });
-    }
-    const repoData = await response.json();
-    res.json(repoData);
-  } catch (error) {
-    res.status(500).json({ message: 'Error fetching repo data', error });
-  }
-});
-
-// Fallback to serve the React app for any other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
-
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+// Serve the React app for any other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
